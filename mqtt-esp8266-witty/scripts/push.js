c4bf7b29423e10b70cd8fd5fdfc8a5d00a165dc5
craigsdennis <craig@teamtreehouse.com>
2016-10-04 21:32:08 -0700

    Necessary changes to upgrade to new thingssdk world

--- mqtt-esp8266-witty/scripts/push.js
+++ mqtt-esp8266-witty/scripts/push.js
@@ -1,11 +1,20 @@
 'use strict';
+const path = require('path');
 const deployer = require('thingssdk-deployer')();
 
 const espruinoStrategy = require('thingssdk-espruino-strategy');
 
 const iotPackageJson = require('../package.json');
 const devices = require('../devices.json').devices;
+const scriptsDir = path.dirname(require.main.filename);
+const payload = {
+    entry: iotPackageJson.main,
+    buildDir: path.join(scriptsDir, '..', 'build')
+};
 
-deployer.build('espruino', espruinoStrategy.build(iotPackageJson.main));
+deployer.prepare(devices, payload);
 
-deployer.upload('espruino', espruinoStrategy.upload(devices));
+deployer.use('espruino', espruinoStrategy.build);
+deployer.use('espruino', espruinoStrategy.upload);
+
+deployer.deploy();
